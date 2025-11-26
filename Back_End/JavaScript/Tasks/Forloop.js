e947e544146adc5c73f9ceadd57673e280366ba2
V-Jagath <jagath9360@gmail.com>
2025-11-26 21:48:02 +0530

    Task Submited

--- Back_End/JavaScript/Tasks/Forloop.js
+++ Back_End/JavaScript/Tasks/Forloop.js
@@ -19,10 +19,44 @@
 // 2.Reverse Triangle
 
 
-for (let a = 5; a >= 5; a--) {
+// for (let a = 5; a >= 1; a--) {
+//     let row = ""
+//     for (let i = 1; i <= a; i++) {
+//         row += "*"
+//     }
+//     console.log(row)
+// }
+
+// *****
+// ****
+// ***
+// **
+// *
+
+
+// 3. Number pyramid
+
+for (let n = 1; n <= 5; n++) {
     let row = ""
-    for (let i = 5; i <= a; i--) {
-        row += "*"
+    for (let a = 1; a <= n; a++) {
+        row += a
     }
     console.log(row)
 }
+
+// 1
+// 12
+// 123
+// 1234
+// 12345
+
+//4. Squre shape star
+
+for (let a = 1; a <= 3; a++) {
+    row = ""
+    for (let i = 1; i <= 5; i++) {
+        row += "*"
+    }
+}
+
+
